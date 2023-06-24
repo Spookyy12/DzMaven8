2e12cb0a7e92e666fbdfc2201a92bea04a5ae0e0
Spookyy12 <131680995+Spookyy12@users.noreply.github.com>
2023-06-24 12:18:13 +0300

    3

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -2,6 +2,8 @@ package ru.netology.statistic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StatisticsService service = new StatisticsService();
+
+
     }
 }
